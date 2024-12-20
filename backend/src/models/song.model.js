a8d8af4d5b91a09b8abd81ece0b80ed82eb9e3f3
Christiana Apinoko <122893327+Shvayne@users.noreply.github.com>
2024-12-20 10:52:41 +0100

    implement routes for getting All albums and by Id

--- backend/src/models/song.model.js
+++ backend/src/models/song.model.js
@@ -28,4 +28,4 @@ const songSchema = new mongoose.Schema({
   }
 }, { timestamps: true});
 
-export const song = mongoose.model("Song", songSchema);
+export const Song = mongoose.model("Song", songSchema);
