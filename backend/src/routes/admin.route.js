954bd964eae7c96d67cd7142f7082806321c19b4
Christiana Apinoko <122893327+Shvayne@users.noreply.github.com>
2024-12-20 10:27:36 +0100

    cleaned up function calls in admin route

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
-import { createSong, deleteSong, createAlbum, deleteAlbum } from "../controller/admin.controller.js";
+import { createSong, deleteSong, createAlbum, deleteAlbum, checkAdmin } from "../controller/admin.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.post("/songs", protectRoute, requireAdmin, createSong);
-router.delete("/songs/:id", protectRoute, requireAdmin, deleteSong);
+router.use(protectRoute, requireAdmin)
 
-router.post("/album", protectRoute, requireAdmin, createAlbum);
-router.delete("/album/:id", protectRoute, requireAdmin, deleteAlbum);
+router.get("/check", checkAdmin);
+
+router.post("/songs", createSong);
+router.delete("/songs/:id", deleteSong);
+
+router.post("/album", createAlbum);
+router.delete("/album/:id", deleteAlbum);
+
 export default router;
