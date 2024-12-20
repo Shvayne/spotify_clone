d4a984e94a6c18513dfd38e25fa3a7f133870ad6
Christiana Apinoko <122893327+Shvayne@users.noreply.github.com>
2024-12-20 10:07:26 +0100

    create logic for deleting and creating entire albums

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createSong } from "../controller/admin.controller.js";
+import { createSong, deleteSong, createAlbum, deleteAlbum } from "../controller/admin.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
-import { deleteSong } from "../controller/admin.controller.js";
 
 const router = Router();
 
 router.post("/songs", protectRoute, requireAdmin, createSong);
 router.delete("/songs/:id", protectRoute, requireAdmin, deleteSong);
+
+router.post("/album", protectRoute, requireAdmin, createAlbum);
+router.delete("/album/:id", protectRoute, requireAdmin, deleteAlbum);
 export default router;
